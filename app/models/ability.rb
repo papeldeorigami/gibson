96d54614bc3fc9f82777adf66b612d7f59ae57da
Ricardo Keigo <papeldeorigami@googlemail.com>
2013-02-05 02:50:18 -0200

    define abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,9 @@ class Ability
   def initialize(subject)
     super
 
-    # Add your authorization rules here
-    # For instance:
-    #    can :create, Comment
-    #    can [:create, :destroy], Post do |p|
-    #      p.actor_id == Actor.normalize_id(subject)
-    #    end
+    can :create, Comment
+    can [:create, :destroy], Post do |p|
+      p.actor_id == Actor.normalize_id(subject)
+    end
   end
 end
